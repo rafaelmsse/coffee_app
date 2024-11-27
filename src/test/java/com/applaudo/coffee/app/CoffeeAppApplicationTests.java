5a3cc99edfa4557f8a5dcc5d5b731fde1bba29c9
Rafael Santos <rafaelmsse@gmail.com>
2024-11-27 17:55:17 -0300

    Initial commit

---
+++ src/test/java/com/applaudo/coffee/app/CoffeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applaudo.coffee.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
